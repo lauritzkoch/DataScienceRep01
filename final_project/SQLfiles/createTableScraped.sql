4c147634e4be2451ba5874c68ec290c615cf3680
Daniel Stephensen <daniel@stephensen.net>
2021-06-17 16:23:20 +0200

    we made it

--- final_project/SQLfiles/createTableScraped.sql
+++ final_project/SQLfiles/createTableScraped.sql
@@ -1,4 +1,4 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS public.scraped_article(id integer,  content text, title text COLLATE pg_catalog.\"default\", release_date date COLLATE pg_catalog.\"default\", url text,sources text) WITH (OIDS = FALSE) TABLESPACE pg_default;
+CREATE TABLE IF NOT EXISTS public.scraped_article(id integer, content text, title text COLLATE pg_catalog.\"default\", release_date date COLLATE pg_catalog.\"default\", url text,sources text) WITH (OIDS = FALSE) TABLESPACE pg_default;
 COMMIT TRANSACTION;
 
